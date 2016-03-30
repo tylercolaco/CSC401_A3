9476689209a981d3a707b14c7663d4326df9a5b2
Tyler Colaco <tylercolaco@gmail.com>
2016-03-30 14:45:43 -0400

    did initial directory stuff

--- gmmClassify.m
+++ gmmClassify.m
@@ -1,3 +1,9 @@
 function result = gmmClassify( dir_test, theta )
+files = dir(strcat(dir_test,'/*.mfcc'));
 
+%to get each data file load
+test_file = load(strcat(dir_test,'/',files(1).name));
+
+
+result = 1;
 end
