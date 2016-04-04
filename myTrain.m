32b6d5b71c80a350ad47b8ff9cfe7e38c0a9a035
dougiechong <dougiechong@gmail.com>
2016-04-04 13:40:43 -0400

    put all beginning and end times of each phoneme into a struct

--- myTrain.m
+++ myTrain.m
@@ -3,6 +3,7 @@ dir_train = 'Training';
 files = dir(dir_train);
 
 count = 0;
+hmms = struct();
 for i=1:length(files)
     if (strcmp(files(i).name,'.') || strcmp(files(i).name,'..'))
     else
@@ -14,19 +15,39 @@ for i=1:length(files)
         gmms{count} = struct();
         gmms{count}.name = speaker;
         %Get the phn files from directories (used to train gausians)
-        
         phn_files = dir(strcat(speaker_data_path,'*.phn'));
         for j=1:length(phn_files)
             fid = fopen(strcat(speaker_data_path,phn_files(j).name));
             chr = fscanf(fid,'%c');
             tmp = textscan(chr, '%s');
+            arr = tmp{1};
+            for k=1:(length(arr)/3)
+                if(strcmp(arr(k*3),'h#'))
+                    %check if field already
+                    if(isfield(hmms, 'sil'))
+                        hmms.('sil'){1} = [hmms.('sil'){1}, [str2num(arr{k*3-2}), str2num(arr{k*3-1})]];
+                    else 
+                        hmms.('sil') = {[str2num(arr{k*3-2}), str2num(arr{k*3-1})]};
+                    end
+                else
+                    if(isfield(hmms, arr{k*3}))
+                        hmms.(arr{k*3}){1} = [hmms.(arr{k*3}){1}, [str2num(arr{k*3-2}), str2num(arr{k*3-1})]];
+                    else 
+                        hmms.(arr{k*3}) = {[str2num(arr{k*3-2}), str2num(arr{k*3-1})]};
+                    end
+                end
+            end
             fclose(fid);
-            disp(chr);
+            %disp(tmp);
             %phn_file = load(strcat(speaker_data_path,phn_files(j).name));
             %X = [X;mfcc_file];
         end
-        disp(X);
+        %disp(X);
     end
 end
+
+%first need to make a model for each phoneme
+
+%next go through each test sequence and determine log likelihood.
 %data = 1;
-%hmm = initHMM(8);
+hmm = initHMM(hmms.sil, 8);
