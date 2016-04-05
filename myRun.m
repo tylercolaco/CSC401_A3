7bfc493d95b7af90431d7730a973470f7faf9496
dougiechong <dougiechong@gmail.com>
2016-04-04 21:28:06 -0400

    run script should work now too

--- myRun.m
+++ myRun.m
@@ -1,6 +1,7 @@
 %dir_train = '/u/cs401/speechdata/Testing';
 dir_test = 'Testing';
 files = dir(strcat(dir_test,'/*.phn'));
+mfccfiles = dir(strcat(dir_test,'/*.mfcc'));
 names = {files.name};
 
 % Loop through all test, find top 5 highest likelihoods
@@ -8,48 +9,46 @@ hmms = struct();
 max_ll = -1000; %something largely negative
 correct = 0;
 incorrect = 0;
+fields = fieldnames(hmmsAfterTrain);
+%for i=1:length(files)
 for i=1:2
     if (strcmp(files(i).name,'.') || strcmp(files(i).name,'..'))
     else
-        
-        count = {};
-        speaker = files(i).name;
-        X = [];
-        
+        X = load(strcat(dir_test, '/', mfcc_files(i).name));
         fid = fopen(strcat(dir_test, '/', files(i).name));
+        disp(phn_files(i).name);
+        disp(mfcc_files(i).name);
         chr = fscanf(fid,'%c');
         tmp = textscan(chr, '%s');
         arr = tmp{1};
         fclose(fid);
-        
-        mfcc_files = dir(strcat(speaker_data_path,'*.mfcc'));
-        for j=1:length(mfcc_files)
-            mfcc_file = load(strcat(speaker_data_path,mfcc_files(j).name));
-            X = [X;mfcc_file];
-        end
-        
-        fields = fieldnames(hmmsAfterTrain);
 
         for k=1:length(arr)/3
-            dat = [str2num(arr{k*3-2}), str2num(arr{k*3-1})];
-            max_ll = -1000;
-            for j=1:numel(fields)
-                ll = loglikHMM(hmmsAfterTrain.(fields{j}), dat);
-                if(ll > max_ll)
-                    %disp(guess);
-                    %disp(fields{i});
-                    %disp(ll);
-                    max_ll = ll;
-                    guess = fields{j};
+            strt = str2num(arr{k*3-2})/128+1;
+            finish = str2num(arr{k*3-1})/128-1;
+            for row=strt:finish
+                max_ll = -1000;
+                for j=1:numel(fields)
+                    ll = loglikHMM(hmmsAfterTrain.(fields{j}), X(row,:));
+                    if(ll > max_ll)
+                        %disp(guess);
+                        %disp(fields{i});
+                        %disp(ll);
+                        max_ll = ll;
+                        guess = fields{j};
+                    end
                 end
-            end
-            disp(guess);
-            disp(arr{k*3});
-            disp(strcmp(guess, arr{k*3}));
-            if(strcmp(guess, arr{k*3}))
-                correct = correct + 1;
-            else
-                incorrect = incorrect + 1;
+                if(strcmp(guess, 'sil'))
+                    guess = 'h#';
+                end
+                disp(guess);
+                disp(arr{k*3});
+                disp(strcmp(guess, arr{k*3}));
+                if(strcmp(guess, arr{k*3}))
+                    correct = correct + 1;
+                else
+                    incorrect = incorrect + 1;
+                end
             end
         end
     end
